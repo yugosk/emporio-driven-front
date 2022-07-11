bed30ab389034ecb034ee494a9da2ea99aaf790a
Yugo <yugosakamoto0717@gmail.com>
2022-07-11 04:04:43 -0300

    imports corrected

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ import NavMenu from "./Navbar/Nav_menu.jsx";
 import MyAcount from "./MyAcount/MyAcount.jsx";
 import Adress from "./Adress/Adress.jsx";
 import Us from "./Us/Us.jsx";
-import AdressContext from "./contexts/AdressContext";
-import UserContext from "./contexts/UserContext";
-import CartContext from "./contexts/CartContext";
-import Home from "../components/Home/Home.jsx";
+import AdressContext from "../contexts/AdressContext";
+import UserContext from "../contexts/UserContext";
+import CartContext from "../contexts/CartContext";
+import Home from "./Home/Home";
 import CategoryPages from "./CategoryPages/CategoryPages.jsx";
 import Cart from "./ShoppingCart/ShoppingCart.jsx";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import ProductPage from "./ProductPage/ProductPage.jsx";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export default function App() {
   const [dataadress, setDataAdress] = useState([]);
